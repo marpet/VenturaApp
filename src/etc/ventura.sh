31b61d454a86b94ce884a5f26c1dc037d10b3971
Marco Peters <marco.peters@brockmann-consult.de>
2023-01-31 12:18:17 +0100

    Initial commit

---
+++ src/etc/ventura.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the path to the Java executable
+JAVA_EXE="/usr/bin/java"
+
+# Set the path to the jar file
+JAR_FILE="/path/to/your.jar"
+
+# Invoke the Java application
+$JAVA_EXE -jar $JAR_FILE
