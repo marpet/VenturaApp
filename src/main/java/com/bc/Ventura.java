dca1e0ce5d5681d9c370025fcd142751619957f2
Marco Peters <marco.peters@brockmann-consult.de>
2023-01-31 15:14:38 +0100

    Added dialog to show system properties  removed printing of properties

--- src/main/java/com/bc/Ventura.java
+++ src/main/java/com/bc/Ventura.java
@@ -1,13 +1,19 @@
 package com.bc;
 
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -16,6 +22,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Ventura {
     public static final String PROP_SETTINGS_FILE = "settingsFile";
@@ -56,11 +67,6 @@ public class Ventura {
                 System.getProperties().load(inStream);
             }
         }
-        if (DEBUG) {
-            System.out.println("--System Properties");
-            System.getProperties().list(System.out);
-            System.out.println("--");
-        }
     }
 
     private static void addMainMenu(JFrame frame) {
@@ -82,7 +88,55 @@ public class Ventura {
         toolsMenu.add(createMenuItem("Tools 1"));
         toolsMenu.add(createMenuItem("Tools 2"));
         toolsMenu.add(createMenuItem("Tools 3"));
-        otherMenu.add(createMenuItem("Help"));
+        JMenu helpMenu = new JMenu("Help");
+        menuBar.add(helpMenu);
+        helpMenu.add(createMenuItem("About"));
+        helpMenu.add(createMenuItem("Properties", a -> showProperties()));
+    }
+
+    private static void showProperties() {
+        JTable table = new JTable(createSystemPropertiesTableModel()) {
+            private static final long   serialVersionUID    = 4957089825220999913L;
+
+            @Override
+            public Component prepareRenderer(TableCellRenderer renderer,
+                                             int row, int column) {
+                Component c = super.prepareRenderer(renderer, row, column);
+                if (c instanceof JComponent) {
+                    JComponent jc = (JComponent) c;
+                    String s = getValueAt(row, column).toString();
+                    jc.setToolTipText(s);
+                }
+                return c;
+            }
+        };
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setSize(200, 300);
+        JFrame jFrame = new JFrame("Properties");
+        jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        jFrame.add(scrollPane);
+        jFrame.pack();
+        jFrame.setVisible(true);
+    }
+
+    private static DefaultTableModel createSystemPropertiesTableModel() {
+        DefaultTableModel model = new DefaultTableModel();
+
+        model.addColumn("Property");
+        model.addColumn("Value");
+
+        Properties p = System.getProperties();
+        Set<Object> keys = p.keySet();
+        SortedSet<Object> sortedKeys = new TreeSet<Object>(keys);
+
+        for (Object sortedKey : sortedKeys) {
+            String key = sortedKey.toString();
+            String value = p.getProperty(key);
+            String[] row = {key, value};
+            model.addRow(row);
+        }
+
+        return model;
     }
 
     private static JMenuItem createMenuItem(String itemName, ActionListener al) {
